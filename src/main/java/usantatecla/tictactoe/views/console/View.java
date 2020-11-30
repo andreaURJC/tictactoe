cba38dc43093bd0479a1a739ea1782ce9793a033
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-11-30 21:47:49 +0100

    *Implement ControllerVisitor

--- src/main/java/usantatecla/tictactoe/views/console/View.java
+++ src/main/java/usantatecla/tictactoe/views/console/View.java
@@ -1,49 +1,51 @@
 package usantatecla.tictactoe.views.console;
 
-import usantatecla.tictactoe.controllers.Controller;
-import usantatecla.tictactoe.controllers.PlayController;
-import usantatecla.tictactoe.controllers.ResumeController;
-import usantatecla.tictactoe.controllers.StartController;
+import usantatecla.tictactoe.controllers.*;
 
-public class View extends usantatecla.tictactoe.views.View {
+public class View extends usantatecla.tictactoe.views.View implements ControllerVisitor {
 
-	private StartView startView;
-	private PlayView playView;
-	private ResumeView resumeView;
+    private StartView startView;
+    private PlayView playView;
+    private ResumeView resumeView;
 
-	public View(StartController startController, PlayController playController, ResumeController resumeController) {
-		this.startView = new StartView(startController);
-		this.playView = new PlayView(playController);
-		this.resumeView = new ResumeView(resumeController);
-	}
+    public View(StartController startController, PlayController playController, ResumeController resumeController) {
+        this.startView = new StartView(startController);
+        this.playView = new PlayView(playController);
+        this.resumeView = new ResumeView(resumeController);
+    }
 
-	public void interact(Controller controller) {}
+    public void interact(Controller controller) {
 
-	@Override
-	protected void start(){
-		this.startView.interact();
-	}
+    }
 
-	@Override
-	protected void play(){
-		this.playView.interact();
-	}
+    @Override
+    protected void start() {
+        this.startView.interact();
+    }
 
-	@Override
-	protected boolean isResumed(){
-		return this.resumeView.interact();
-	}
+    @Override
+    protected void play() {
+        this.playView.interact();
+    }
 
-	public void visit(StartController startController) {
-		this.startView.interact();
-	}
+    @Override
+    protected boolean isResumed() {
+        return this.resumeView.interact();
+    }
 
-	public void visit(PlayController playController) {
-		this.playView.interact();
-	}
+    @Override
+    public void visit(StartController startController) {
+        this.startView.interact();
+    }
 
-	public void visit(ResumeController resumeController) {
-		this.resumeView.interact();
-	}
+    @Override
+    public void visit(PlayController playController) {
+        this.playView.interact();
+    }
+
+    @Override
+    public void visit(ResumeController resumeController) {
+        this.resumeView.interact();
+    }
 
 }
