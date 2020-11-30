5007191e30d47a11faaf3de9eb0f39fb676df921
andreacolina <a.colina.2020@alumnos.urjc.es>
2020-11-30 20:06:21 +0100

    Added getStateValue() to Session

--- src/main/java/usantatecla/tictactoe/models/Session.java
+++ src/main/java/usantatecla/tictactoe/models/Session.java
@@ -1,4 +1,15 @@
 package usantatecla.tictactoe.models;
 
+import usantatecla.tictactoe.types.StateValue;
+
 public class Session {
+    private State state;
+
+    public Session() {
+        this.state = new State();
+    }
+
+    public StateValue getStateValue() {
+        return this.state.getStateValue();
+    }
 }
